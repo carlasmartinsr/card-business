2217a3d9930bb66df7ff5f8671fca66c99508c0f
Carla <107934506+carlasmartinsr@users.noreply.github.com>
2024-06-02 01:57:02 +0300

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer(){
+    return(
+        <footer>
+            <nav className="card-footer-nav" aria-label="Social Media Profiles">
+                <a href=" https://x.com/CarlasMartins?t=a6bG8M2eaou9trsfgQnmIg&s=08" target="_blank"  rel="noopener noreferrer" className="icon-social fa-brands fa-square-x-twitter">
+                    <span className="sr-only">Twitter (Opens in a new tab)</span>
+                </a>
+                <a href="https://www.facebook.com/carla.martinsrodriguez/" target="_blank"  rel="noopener noreferrer"  className="icon-social fa-brands fa-square-facebook">
+                    <span className="sr-only">Facebook(Opens in a new tab)</span>
+                </a>
+                <a href="https://www.instagram.com/carlamartinsrodriguez/" target="_blank"  rel="noopener noreferrer"  className="icon-social fa-brands fa-square-instagram">
+                    <span className="sr-only"> Instagram (Opens in a new tab)</span>
+                </a>
+                <a href="https://github.com/carlasmartinsr" target="_blank"   rel="noopener noreferrer" className=" icon-social fa-brands fa-square-github">
+                    <span className="sr-only"> Github (Opens in a new tab)</span>
+                </a>
+            </nav>
+       </footer>
+    )
+}
+
+export default Footer
